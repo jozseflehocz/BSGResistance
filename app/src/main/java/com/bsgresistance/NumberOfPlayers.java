b3e28738269fd0d4e33b32641415b15dc0fdb426
jlehocz <jozsef@lehocz.hu>
2018-08-12 13:33:07 +0200

    + changeNumberOfPlayers

--- app/src/main/java/com/bsgresistance/NumberOfPlayers.java
+++ app/src/main/java/com/bsgresistance/NumberOfPlayers.java
@@ -2,10 +2,13 @@ package com.bsgresistance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.TextView;
@@ -14,12 +17,15 @@ import com.bsgresistance.bsgresistance.R;
 
 public class NumberOfPlayers extends AppCompatActivity {
 
+    Button increaseNumberOfPlayersButton;
+    Button decreaseNumberOfPlayersButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number_of_players);
 
-        TextView tudosModuleHeader= findViewById(R.id.tudos_modul_header);
+        TextView tudosModuleHeader = findViewById(R.id.tudos_modul_header);
         final GridLayout tudosModuleGrid = findViewById(R.id.tudos_modul_grid);
         tudosModuleHeader.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,7 +34,7 @@ public class NumberOfPlayers extends AppCompatActivity {
             }
         });
 
-        TextView moralisModuleHeader= findViewById(R.id.moralis_modul_header);
+        TextView moralisModuleHeader = findViewById(R.id.moralis_modul_header);
         final GridLayout moralisModuleGrid = findViewById(R.id.moralis_modul_grid);
         moralisModuleHeader.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,8 +43,21 @@ public class NumberOfPlayers extends AppCompatActivity {
             }
         });
 
+        increaseNumberOfPlayersButton = findViewById(R.id.increase_number_of_players);
+        increaseNumberOfPlayersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeNumberOfPlayers(1);
+            }
+        });
 
-
+        decreaseNumberOfPlayersButton = findViewById(R.id.decrease_number_of_players);
+        decreaseNumberOfPlayersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeNumberOfPlayers(-1);
+            }
+        });
     }
 
     @Override
@@ -65,14 +84,39 @@ public class NumberOfPlayers extends AppCompatActivity {
 
     /**
      * Set visibility of the selected module. If visible set to hidden. If hidden set to visible
+     *
      * @param moduleGrid
      */
 
-    private void setModuleVisibility(GridLayout moduleGrid ){
-        if (moduleGrid.getVisibility()==View.VISIBLE){
+    private void setModuleVisibility(GridLayout moduleGrid) {
+        if (moduleGrid.getVisibility() == View.VISIBLE) {
             moduleGrid.setVisibility(View.GONE);
-        }else{
+        } else {
             moduleGrid.setVisibility(View.VISIBLE);
         }
+    }
+
+    /**
+     * Increase or decrease the number of players, and disable enable buttons depending on
+     * the current value
+     * @param changeValue
+     */
+
+    private void changeNumberOfPlayers(int changeValue) {
+        EditText numberOfPlayers = findViewById(R.id.number_of_players);
+        int currentNumberOfPlayers = Integer.parseInt(numberOfPlayers.getText().toString().trim());
+
+        decreaseNumberOfPlayersButton.setEnabled(true);
+        increaseNumberOfPlayersButton.setEnabled(true);
+        currentNumberOfPlayers = currentNumberOfPlayers+changeValue;
+        numberOfPlayers.setText(String.valueOf(currentNumberOfPlayers));
+
+        if (currentNumberOfPlayers<6) {
+            decreaseNumberOfPlayersButton.setEnabled(false);
+        }
+        if (currentNumberOfPlayers>11) {
+            increaseNumberOfPlayersButton.setEnabled(false);
+        }
+
     }
 }
